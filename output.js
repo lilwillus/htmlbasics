064cb598ee13d86bdf0d17c3835cc92a39ef479a
lilwillus <williamhathawayjr@gmail.com>
2018-02-15 20:56:09 -0600

    output.js

---
+++ output.js
@@ -0,0 +1,9 @@
+function consolePrint(){
+    console.log("hello William");
+}
+function alertPrint(){
+    alert("hello William");
+}
+function documentPrint(){
+    document.write("hello William")
+}
