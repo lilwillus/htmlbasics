1832c0a29206066961f90b20990f225dfe2cf41e
lilwillus <williamhathawayjr@gmail.com>
2018-02-27 12:11:01 -0600

    sum.html

---
+++ sum.js
@@ -0,0 +1,24 @@
+
+function sum(){
+var num1 = document.getElementById("num1").value;
+var num2 = document.getElementById("num2").value
+alert (parseInt(num1) + parseInt(num2));
+}
+
+function difference(){
+var num3 = document.getElementById("num3").value;
+var num4 = document.getElementById("num4").value
+alert (parseInt(num3) - parseInt(num4));
+}
+
+function product(){
+var num5 = document.getElementById("num5").value;
+var num6 = document.getElementById("num6").value
+alert (parseInt(num5) * parseInt("num6"));
+}
+
+function dividend(){
+var num7 = document.getElementById("num7").value;
+var num8 = document.getElementById("num8").value
+alert (parseFloat(num7) / parseInt("num8"));
+}
